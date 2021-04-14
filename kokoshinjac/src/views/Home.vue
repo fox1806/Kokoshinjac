7229c0d92c642c84b09546b59ae9ef1565af1170
Alen Abdulahović <alen.abdulahovic1@gmail.com>
2021-04-14 17:37:11 +0200

    Inicijalna verzija aplikacije (kostur sa routerom)

---
+++ kokoshinjac/src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Kokoshinjac</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+  .home{
+    position: absolute;
+    width: 100vh;
+    height: 150px;
+    background-color: black;
+    top: 0;
+    left: 0;
+  }
+  
+  .home img{
+    height: 110px;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: center;
+    padding-top: 20px;
+    padding-left: 20vh;
+    
+  }
+
+  .home h1{
+    color: #D4AF37;
+    position: absolute;
+    top: 0;
+    padding-top: 10px;
+    left: 30vh;
+    font-size: 45px;
+  }
+</style>
