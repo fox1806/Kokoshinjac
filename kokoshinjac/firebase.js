e529b655ef2e011dda88e42a4c35be33d2a7ef9b
fox1806 <phoenixdipf@gmail.com>
2021-05-04 12:28:26 +0200

    Update firebase.js

--- kokoshinjac/firebase.js
+++ kokoshinjac/firebase.js
@@ -4,12 +4,7 @@ import "firebase/auth";
 
 const firebaseConfig = {
     // unos podataka API-a
-    apiKey: "AIzaSyDHXzbJXw6J5Hhxo5KZetYYnIBRbpEzXyg",
-    authDomain: "kokoshinjac-ce239.firebaseapp.com",
-    projectId: "kokoshinjac-ce239",
-    storageBucket: "kokoshinjac-ce239.appspot.com",
-    messagingSenderId: "350418679609",
-    appId: "1:350418679609:web:fdf4bca0235cd873d4dae8"
+    
   };
   
   // Initialize Firebase
